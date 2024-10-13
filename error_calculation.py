Fix normalizing before MSE calculation

error_calculation.py
@@ -14,17 +14,19 @@ class MeanSquareErrorCalculator:
         self.sorted_counter = OrderedDict(sorted(self.counter.items(), key=lambda x: x[0]))
 
     def calculate_mse_normal(self, mean, std_dev):
-        p = norm.pdf(list(self.sorted_counter.keys()), mean, std_dev) * self.num_balls
+        p = norm.pdf(list(self.sorted_counter.keys()), mean, std_dev)
 
-        diff = np.array(list(self.sorted_counter.values())) - p
+        normalized_counter = np.array(list(self.sorted_counter.values())) / self.num_balls
+        diff = normalized_counter - p
         squared_diff = diff ** 2
         mse = np.mean(squared_diff)
         return mse
 
     def calculate_mse_binomial(self):
-        binom_pmf = binom.pmf(list(self.sorted_counter.keys()), self.levels, 0.5) * self.num_balls
+        binom_pmf = binom.pmf(list(self.sorted_counter.keys()), self.levels, 0.5)
 
-        diff = np.array(list(self.sorted_counter.values())) - binom_pmf
+        normalized_counter = np.array(list(self.sorted_counter.values())) / self.num_balls
+        diff = normalized_counter - binom_pmf
         squared_diff = diff ** 2
         mse = np.mean(squared_diff)
         return np.sqrt(mse)